f8cb74c33ef0886006b96b22ce765770d00a7309
Kashoid23 <kashoyid@gmail.com>
2026-02-07 01:09:10 +0200

    Show Sign Up or Login link depending on currentPathname

--- client/app/src/components/Header.js
+++ client/app/src/components/Header.js
@@ -1,18 +1,23 @@
 'use client'
 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import { useAuth } from '../context/auth';
 
 export default function Header() {
     const { currentUser } = useAuth();
+    const currentPathname = usePathname();
 
     return currentUser ? (
         <Link href="/auth/logout" className="btn btn-secondary">Logout</Link>
     ) : (
         <div>
-            <Link href="/auth/login" className="btn btn-primary">Login</Link>
-            <Link href="/auth/signup" className="btn btn-secondary ms-2">Sign Up</Link>
+            {currentPathname === "/auth/login" ? (
+                <Link href="/auth/signup" className="btn btn-primary">Sign Up</Link>
+            ) : (
+                <Link href="/auth/login" className="btn btn-primary">Login</Link>
+            )}
         </div>
     )
 };
